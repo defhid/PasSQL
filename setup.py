4f96346bb4cdd903828a7e37708e3797aa4e1988
Vladislav <neo_120@mail.ru>
2021-12-03 22:05:46 +0300

    pypi package setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+def setup():
+    from setuptools import setup
+
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setup(name='passql',
+          version='0.1',
+          description='Super light ORM',
+          long_description=long_description,
+          packages=['passql'],
+          author='Vlad Mironov',
+          author_email='hidden120@mail.ru',
+          python_requires='>=3.6',
+          zip_safe=False,
+          classifiers=[
+                "Programming Language :: Python :: 3.8",
+                "License :: OSI Approved :: MIT License",
+                "Operating System :: OS Independent",
+          ])
+
+
+def publish():
+    from twine.commands import upload
+
+    upload.main(['dist\*'])
+
+
+if __name__ != '__main__':
+    # python setup.py sdist
+    publish()
+else:
+    setup()
