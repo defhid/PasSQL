95399076901195857829438a1aceac9dbacc3009
Vladislav <neo_120@mail.ru>
2021-12-06 19:17:45 +0300

    publishing optimized

--- setup.py
+++ setup.py
@@ -1,43 +1,67 @@
 """ python setup.py sdist """
 
-from setuptools import setup
-from twine.commands import upload
-from shutil import rmtree
+INFO = {
+    'name': "passql",
+    'version': "0.2.2",
 
-print("## CREATING DIST FILES...")
+    'description': "Super light ORM",
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+    'author': "Vladislav Mironov",
+    'author_email': "hidden120@mail.ru",
 
-setup(
-    name='passql',
-    version='0.2.1',
-
-    description='Super light ORM',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-
-    author='Vladislav Mironov',
-    author_email='hidden120@mail.ru',
-
-    packages=['passql'],
-    zip_safe=False,
-
-    python_requires='>=3.6',
-    classifiers=[
+    'python_requires': ">=3.6",
+    'classifiers': [
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ]
-)
+}
 
-print("## PUBLISHING...")
 
-upload.main(['dist\*'])
+def make_dist_files():
+    from setuptools import setup
 
-print("## REMOVING DIST FILES...")
+    print("## CREATING DIST FILES...")
 
-rmtree('dist')
-rmtree('passql.egg-info')
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
 
-print("## SUCCESS")
+    setup(
+        **INFO,
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        packages=['passql'],
+        zip_safe=False,
+    )
+
+
+def publish() -> bool:
+    from twine.commands import upload
+
+    print("## PUBLISHING...")
+
+    try:
+        upload.main([r'dist\*'])
+    except Exception as e:
+        print(e)
+        return False
+    else:
+        return True
+
+
+def remove_dist_files():
+    from shutil import rmtree
+
+    print("## REMOVING DIST FILES...")
+
+    rmtree('dist')
+    rmtree('passql.egg-info')
+
+
+make_dist_files()
+
+ok = publish()
+
+remove_dist_files()
+
+print("## " + ("SUCCESS" if ok else "FAILURE"))
